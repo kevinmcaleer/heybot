78f624bcc0312f042d83e0d290ffcc7dba44a4dc
Kevin McAleer <kevinmcaleer@gmail.com>
2022-09-29 10:17:28 +0100

    Initial commit

---
+++ heybot.py
@@ -0,0 +1,28 @@
+# HeyBot! A simple Pomodoro timer and Notification bot that can sit on your desk
+# Kevin McAleer
+# September 2022
+
+
+class Heybot():
+
+    current_time = "" # the current time as a string
+    time_remaining = "" # the remaining time as a string
+
+    def __init__(self):
+        pass
+
+    def alert(self):
+        pass
+
+    def set_timer(self, minutes:int=None):
+        pass
+
+    def timeup(self) -> bool:
+        """ True if the time is up"""
+        pass
+
+    def tick(self):
+        """ Peforms a random animation """
+        pass
+
+    
